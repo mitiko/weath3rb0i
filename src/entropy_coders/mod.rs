a592cc69b4dc30620edc9c259d41db87b56a2789
Dimitar Rusev <mitikodev@gmail.com>
2022-11-09 14:46:54 +0200

    Add cfg-if for cleaner imports

--- src/entropy_coders/mod.rs
+++ src/entropy_coders/mod.rs
@@ -2,19 +2,21 @@
 // Define AC
 // Define rANS?
 // tANS, Huffman with static models?
+mod ac_io;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "ac32")] {
+        pub mod ac32;
+        pub use ac32 as arithmetic_coding;
+    } else if #[cfg(feature = "ac48")] {
+        pub mod ac48;
+        pub use ac48 as arithmetic_coding;
+    } else if #[cfg(feature = "ac64")] {
+        pub mod ac64;
+        pub use ac64 as arithmetic_coding;
+    }
+}
 
 use std::io;
-
-mod ac_io;
-pub mod ac32;
-pub mod ac48;
-pub mod ac64;
-#[cfg(feature = "ac32")]
-pub use ac32 as arithmetic_coding;
-#[cfg(feature = "ac48")]
-pub use ac48 as arithmetic_coding;
-#[cfg(feature = "ac64")]
-pub use ac64 as arithmetic_coding;
 
 pub trait ACEncoder {
     fn encode(&mut self, bit: u8, prob: u16) -> io::Result<()>;
