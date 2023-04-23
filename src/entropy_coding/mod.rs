c45e66591b2f52f2306aa3726f8e53b0e2a907df
Dimitar Rusev <mitikodev@gmail.com>
2023-04-23 21:40:36 +0300

    Refactor arithmetic coder

--- src/entropy_coding/mod.rs
+++ src/entropy_coding/mod.rs
@@ -1,19 +1,15 @@
 pub mod ac_io;
-use self::ac_io::{ACRead, ACReader, ACWrite, ACWriter};
-use std::io::{self, Read, Write};
+use self::ac_io::{ACRead, ACWrite};
+use std::io;
 
-const PRECISION: u32 = u32::BITS; // 32
-const PREC_SHIFT: u32 = PRECISION - 1; // 31
-const Q1: u32 = 1 << (PRECISION - 2); // 0x40000000, 1 = 0b01, quarter 1
-const Q2: u32 = 2 << (PRECISION - 2); // 0x80000000, 2 = 0b10, range mid
-const Q3: u32 = 3 << (PRECISION - 2); // 0xC0000000, 3 = 0b11, quarter 3
+const PREC_SHIFT: u32 = u32::BITS - 1; // 31
+const Q1: u32 = 1 << (PREC_SHIFT - 1); // 0x40000000, 1 = 0b01, quarter 1
+const Q2: u32 = 2 << (PREC_SHIFT - 1); // 0x80000000, 2 = 0b10, range middle
+const Q3: u32 = 3 << (PREC_SHIFT - 1); // 0xC0000000, 3 = 0b11, quarter 3
 const RLO_MOD: u32 = (1 << PREC_SHIFT) - 1; // 0x7FFFFFFF, range low modify
 const RHI_MOD: u32 = (1 << PREC_SHIFT) + 1; // 0x80000001, range high modify
 
-/// The `ArithmeticCoder` compresses bits given a probability and writes
-/// fractional bits to an internal `Write` instance.
-///
-/// See https://en.wikipedia.org/wiki/Arithmetic_coding
+/// The `ArithmeticCoder` encodes/decodes bits given a probability
 pub struct ArithmeticCoder<T> {
     x1: u32, // low
     x2: u32, // high
@@ -26,8 +22,9 @@ impl<W: ACWrite> ArithmeticCoder<W> {
         Self { io: writer, x1: 0, x2: u32::MAX, x: 0 }
     }
 
-    /// Encodes 4-bits at once.
+    /// TODO: Encode 4-bits at once.
     pub fn encode4(&mut self, nib: u8, probs: [u16; 4]) -> io::Result<()> {
+        // todo!();
         self.encode(nib & 0b1000, probs[0])?;
         self.encode(nib & 0b0100, probs[1])?;
         self.encode(nib & 0b0010, probs[2])?;
@@ -102,24 +99,15 @@ impl<R: ACRead> ArithmeticCoder<R> {
     }
 }
 
-// TODO: Verify rounded range, test speed
-// let lerped_range = (range * p) >> (RANGE_SHIFT - 1);
-// let rounded_range = (lerped_range >> 1) + (lerped_range & 1);
-// let xmid = x1 + u32::try_from(rounded_range).unwrap();
 #[inline(always)]
 fn lerp(x1: u32, x2: u32, prob: u16) -> u32 {
-    const P_SHIFT: u32 = u32::BITS - u16::BITS;
-    const RANGE_SHIFT: u32 = P_SHIFT + u16::BITS;
-
-    let mut p = u64::from(prob) << P_SHIFT;
-    p = if p == 0 { 1 } else { p };
-
+    // make prob 32-bit & always leave chance
+    let p = if prob == 0 { 1 } else { u64::from(prob) << 16 };
     let range = u64::from(x2 - x1);
-    let lerped_range = (range * p) >> RANGE_SHIFT;
+    let lerped_range = (range * p) >> 32;
 
-    // never fails, as both range and p < 2^32
+    // no overflows/underflows, as both range < 2^32 and p < 2^32
     let xmid = x1 + u32::try_from(lerped_range).unwrap();
-
     debug_assert!(xmid >= x1 && xmid < x2);
     xmid
 }
