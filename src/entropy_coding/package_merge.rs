3637f7683d02b95df8d7f661b081d987770512c1
Dimitar Rusev <mitikodev@gmail.com>
2024-02-07 23:28:39 +0200

    Refactor main loop

--- src/entropy_coding/package_merge.rs
+++ src/entropy_coding/package_merge.rs
@@ -10,12 +10,16 @@ fn package_merge(counts: &[u32], max_len: u8) -> Vec<u8> {
     let sorted_counts: Vec<_> = symbol2count.iter().map(|&x| x.1).collect();
 
     assert!(sorted_counts.len() != 0, "No symbols provided");
-    assert!(sorted_counts.len() <= 1 << max_len, "Max length is too small");
     assert!(max_len <= 32, "Max length is too big");
+    assert!(
+        sorted_counts.len() <= 1 << max_len,
+        "Max length is too small"
+    );
 
     let sorted_code_lens = package_merge_sorted(&sorted_counts, max_len);
     let mut code_lens = vec![0; counts.len()];
-    symbol2count.iter()
+    symbol2count
+        .iter()
         .map(|x| x.0)
         .zip(sorted_code_lens)
         .for_each(|(sym, code_len)| code_lens[sym] = code_len);
@@ -29,34 +33,31 @@ fn package_merge(counts: &[u32], max_len: u8) -> Vec<u8> {
 fn package_merge_sorted(a: &[u32], max_len: u8) -> Vec<u8> {
     let mut package_depths: Vec<u32> = vec![0; a.len() * 2 - 1];
     let mut prev: Vec<u32> = a.iter().copied().collect();
+    let mut curr = Vec::with_capacity(a.len() * 2 - 1);
 
     for depth in 1..max_len {
         let mask = 1 << depth; // records at which depth it was packaged
-        let mut seq = a.iter().peekable();
+        let mut seq = a.iter().peekable(); // always merge with the initial counts
         let mut packages = prev.chunks_exact(2).map(|x| x[0] + x[1]).peekable();
-        let mut curr = Vec::with_capacity(a.len() + prev.len() / 2 + 2);
+        curr.clear(); //
 
-        // merge packages with original sequence
+        // merge iteration
         loop {
-            // TODO: refactor
-            let (next_item, is_package) = match (seq.peek(), packages.peek()) {
+            let is_package = match (packages.peek(), seq.peek()) {
                 (None, None) => break,
-                (_, None) => (seq.next().copied(), false), // merged all packages
-                (None, _) => (packages.next(), true),      // merged original sequence
-                (Some(&a), Some(b)) => {
-                    if a <= b {
-                        (seq.next().copied(), false)
-                    } else {
-                        (packages.next(), true)
-                    }
-                }
+                (None, _) => false,
+                (_, None) => true,
+                (Some(a), Some(&b)) => a <= b,
             };
-            if is_package {
+            let next_item = if is_package {
                 package_depths[curr.len()] |= mask;
-            }
-            curr.push(next_item.unwrap());
+                packages.next().unwrap()
+            } else {
+                seq.next().copied().unwrap()
+            };
+            curr.push(next_item);
         }
-        prev = curr; // TODO: mem swap for efficiency
+        std::mem::swap(&mut prev, &mut curr);
     }
 
     let mut code_lens = vec![0; a.len()];
@@ -93,11 +94,11 @@ mod tests {
 
     #[test]
     fn stephan_brumme_example() {
-        assert_eq!(package_merge(&[270, 20, 10, 0, 1, 6, 1], 4), [1, 2, 4, 0, 4, 4, 4]);
-        assert_eq!(package_merge(&[10, 20, 270, 0, 1, 6, 1], 4), [4, 2, 1, 0, 4, 4, 4]);
+        let counts = [270, 20, 10, 0, 1, 6, 1];
+        assert_eq!(package_merge(&counts, 4), [1, 2, 4, 0, 4, 4, 4]);
+        let counts = [10, 20, 270, 0, 1, 6, 1];
+        assert_eq!(package_merge(&counts, 4), [4, 2, 1, 0, 4, 4, 4]);
     }
-
-    // TODO: enwik8, book1 frequencies
 
     #[test]
     fn single_symbol() {
