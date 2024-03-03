b5056b2ec8ddde296dcf3d6a7de5b2c0e61d0f13
Dimitar Rusev <mitikodev@gmail.com>
2024-03-03 22:15:40 +0200

    Add order0 binary

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PS3="Select binary to run: "
+binaries=(
+    "order0"
+)
+
+# TODO: if arg == 1, select the first choice
+# TODO: pass release mode
+
+select binaryName in "${binaries[@]}"; do
+    cargo run --bin $binaryName
+    break
+done
