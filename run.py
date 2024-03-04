6b723dab0e95ef368929f3f2baf3ae794f805153
Dimitar Rusev <mitikodev@gmail.com>
2024-03-04 16:39:11 +0200

    Parse binary by name

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ binaries = os.listdir(binaries_path)
 binaries.insert(0, 'weath3rb0i')
 
 # Add all parameters globally but only use the ones we require per binary (no checks for extra args)
-parser.add_argument("bin", type=int, nargs="?", help="Which binary (by id) to run")
+parser.add_argument("bin", nargs="?", help="Which binary (by id) to run")
 parser.add_argument(
     "-r", "--release", action="store_true", help="Whether to compile in release mode"
 )
@@ -24,12 +24,24 @@ parser.add_argument("-q", "--quiet", action="store_true", help="Hide compiler in
 parser.add_argument("--hsize", type=int, help="Max code length for Huffman tree")
 
 args = parser.parse_args()
+if args.bin in binaries:
+    args.bin = binaries.index(args.bin)
+else:
+    try:
+        args.bin = int(args.bin) if args.bin is not None else None
+    except ValueError:
+        args.bin = None
+
 while args.bin is None or args.bin >= len(binaries):
     for i, binary in enumerate(binaries[1:]):
         print(f"{i+1}) {binary}")
 
     try:
-        args.bin = int(input("Select binary to run: "))
+        selected_binary = input("Select binary to run: ")
+        if selected_binary in binaries:
+            args.bin = binaries.index(selected_binary)
+        else:
+            args.bin = int(selected_binary)
     except ValueError:
         continue
 
