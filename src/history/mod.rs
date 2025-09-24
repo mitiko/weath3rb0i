f57e74dafe7c3eb53e30f8b0f6bfbf0c9040f295
Dimitar Rusev <mitikodev@gmail.com>
2025-09-24 11:14:33 +0300

    Add regular entropy writer

--- src/history/mod.rs
+++ src/history/mod.rs
@@ -57,3 +57,40 @@ impl crate::entropy_coding::arithmetic_coder::ACWrite for CappedEntropyWriter {
         unimplemented!("Entropy writer doesn't implement flushing")
     }
 }
+
+#[derive(Clone, Debug)]
+struct EntropyWriter {
+    state: u32,
+    rev_bits: u16,
+}
+
+impl EntropyWriter {
+    fn new() -> Self {
+        Self { state: 0, rev_bits: 0 }
+    }
+}
+
+impl crate::entropy_coding::arithmetic_coder::ACWrite for EntropyWriter {
+    fn write_bit(&mut self, bit: impl TryInto<u8>) -> std::io::Result<()> {
+        use std::io::{Error, ErrorKind};
+        let bit = bit.try_into().unwrap_or_default();
+
+        // self.state = (self.state >> 1) | (u32::from(bit) << 31);
+        self.state = (self.state << 1) | u32::from(bit);
+        while self.rev_bits > 0 {
+            self.rev_bits -= 1;
+            // self.state = (self.state >> 1) | (u32::from(bit ^ 1) << 31);
+            self.state = (self.state << 1) | u32::from(bit ^ 1);
+        }
+
+        Ok(())
+    }
+
+    fn inc_parity(&mut self) {
+        self.rev_bits += 1;
+    }
+
+    fn flush(&mut self, _padding: u32) -> std::io::Result<()> {
+        unimplemented!("Entropy writer doesn't implement flushing")
+    }
+}
